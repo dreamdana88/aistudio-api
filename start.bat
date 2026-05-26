8e35e5a9798293d4b50f9de671a670430a71cd9f
wilderye <13357356227@163.com>
2026-05-26 18:58:55 +0800

    feat: auto pip install on startup

--- start.bat
+++ start.bat
@@ -3,6 +3,16 @@ set CLOAKBROWSER_AUTO_UPDATE=false
 set CLOAKBROWSER_CACHE_DIR=%~dp0.cloakbrowser
 set PYTHONPATH=%~dp0src
 cd /d %~dp0
+echo Installing dependencies...
+python -m pip install -r requirements.txt -q
+if errorlevel 1 (
+    echo.
+    echo Failed to install dependencies. Please make sure Python 3.11+ is installed.
+    echo Download from: https://www.python.org/downloads/
+    pause
+    exit /b 1
+)
+echo.
 echo Starting aistudio-api proxy...
 echo Browser will be downloaded automatically on first run.
 echo If download fails, please manually download from:
